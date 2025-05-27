88562ed1150496c75c395488ef708ef05479e9b7
Anmol Shah <anmolsha80@gmail.com>
2025-05-27 15:56:56 +0545

    features/create-individual-event-page - Add the correct horizontal padding to the section wrapping the EventCard component

--- src/components/events-list.tsx
+++ src/components/events-list.tsx
@@ -10,7 +10,7 @@ const EventsList = ({ events }: EventsListProps) => {
   if (!events || events.length === 0) return null;
 
   return (
-    <section className="max-w-[1100px] flex flex-wrap gap-10 justify-center px-20">
+    <section className="max-w-[1100px] flex flex-wrap gap-10 justify-center px-[20px]">
       {events.map((event) => (
         <EventCard key={event.id} event={event} />
       ))}
