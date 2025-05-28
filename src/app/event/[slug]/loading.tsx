c4907a56ea9038c369997527078b82cfb8551b03
Anmol Shah <anmolsha80@gmail.com>
2025-05-28 22:42:10 +0545

    features/render-skeletons-while-loading - Make the skeleton loading of individual event page responsive for mobile, tablet and desktop devices

--- src/app/event/[slug]/loading.tsx
+++ src/app/event/[slug]/loading.tsx
@@ -3,31 +3,31 @@ import Skeleton from '@/components/skeleton';
 const Loading = () => (
   <div>
     <div className="relative overflow-hidden flex justify-center items-center py-14 md:py-20">
-      <div className="w-[1275px] h-[361px] bg-gray-700/40 z-0 absolute"></div>
+      <div className="h-[500px] w-[1275px] lg:h-[361px] bg-gray-700/40 z-0 absolute"></div>
 
-      <div className="z-50 flex flex-col gap-6 lg:gap-16 lg:flex-row relative">
+      <div className="z-1 flex flex-col gap-6 lg:gap-16 lg:flex-row relative">
         <Skeleton className="h-[198px] w-[297px]" />
 
         <div className="flex flex-col">
           <Skeleton className="h-6 w-[200px]" />
-          <Skeleton className="h-8 w-[406px] my-3" />
+          <Skeleton className="h-8 w-[280px] md:w-[406px] my-3" />
           <Skeleton className="h-7 w-[250px]" />
-          <Skeleton className="h-12 w-[403px] mt-auto" />
+          <Skeleton className="h-12 w-[305px] md:w-[403px] mt-6 lg:mt-auto" />
         </div>
       </div>
     </div>
 
     <div className="min-h-[75vh] flex flex-col items-center px-5 py-16">
       <div className="mb-12">
-        <Skeleton className="mb-5 mx-auto h-6 w-[250px]" />
-        <Skeleton className="mb-5 mx-auto h-4 w-[750px]" />
-        <Skeleton className="mb-5 mx-auto h-4 w-[650px]" />
-        <Skeleton className="mb-5 mx-auto h-4 w-[550px]" />
+        <Skeleton className="mb-5 mx-auto h-6 w-[175px] md:w-[250px]" />
+        <Skeleton className="mb-5 mx-auto h-4 w-[300px] md:w-[750px]" />
+        <Skeleton className="mb-5 mx-auto h-4 w-[225px] md:w-[650px]" />
+        <Skeleton className="mb-5 mx-auto h-4 w-[175px] md:w-[550px]" />
       </div>
 
       <div className="mb-12">
-        <Skeleton className="mb-5 mx-auto h-6 w-[150px]" />
-        <Skeleton className="mb-5 mx-auto h-4 w-[300px]" />
+        <Skeleton className="mb-5 mx-auto h-6 w-[125px] md:w-[150px]" />
+        <Skeleton className="mb-5 mx-auto h-4 w-[250px] md:w-[300px]" />
       </div>
     </div>
   </div>
