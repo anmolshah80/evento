62c2eb110e3dea89d2989198041477ca439417fd
Anmol Shah <anmolsha80@gmail.com>
2025-05-30 22:52:28 +0545

    features/install-and-configure-prisma-orm - Create a not-found-graphic-ui.tsx component to render a 404 Not Found graphic with the same text - Refactor not-found.tsx component to render a UI when a non-existing url is accessed

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,5 +1,37 @@
+import Link from 'next/link';
+import { ArrowTopLeftIcon } from '@radix-ui/react-icons';
+
+import NotFoundGraphicUI from '@/components/not-found-graphic-ui';
+
 const NotFound = () => {
-  return <main>We could not find that page</main>;
+  debugger;
+
+  return (
+    <main className="grid min-h-full place-items-center px-6 py-24 sm:py-32 lg:px-8">
+      {/*
+        There is no support for metadata in not-found.tsx yet
+          - https://github.com/vercel/next.js/issues/45620#issuecomment-1488933853
+          - https://github.com/vercel/next.js/pull/47328#issuecomment-1488891093
+      */}
+      <title>404 Not Found</title>
+      <div className="text-center">
+        <NotFoundGraphicUI />
+
+        <p className="mt-6 text-xl text-pretty text-gray-500 sm:text-2xl">
+          Sorry, there is nothing at this URL.
+        </p>
+        <div className="mt-12 flex items-center justify-center gap-x-6">
+          <Link
+            href={'/'}
+            className="flex gap-x-2 text-xl text-white/80 hover:underline hover:text-white/100 focus:text-white/100 focus:underline outline-none"
+          >
+            <ArrowTopLeftIcon width={20} height={20} />
+            Go back home
+          </Link>
+        </div>
+      </div>
+    </main>
+  );
 };
 
 export default NotFound;
